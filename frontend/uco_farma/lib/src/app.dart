7891ae6012cea9e9696a5b229e385f2cabfdffef
Rafael García Pérez <p82garpr@uco.es>
2024-11-13 20:45:18 +0100

    theme added

---
+++ frontend/uco_farma/lib/src/app.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:uco_farma/src/config/theme/app_theme.dart';
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Uco Farma',
+      debugShowCheckedModeBanner: false, // Remove the debug banner
+      theme: ThemeData(
+        colorScheme: AppTheme().theme().colorScheme,
+        useMaterial3: true,
+      ),
+    );
+  }
+
+}
---
+++ frontend/uco_farma/lib/src/config/theme/app_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+//const Color _customColor = Color(0xFF6200EE);
+
+const List<Color> _colorThemes = [
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.indigo,
+];
+//There is  6 colors in the list _colorThemes
+
+class AppTheme{
+  ThemeData theme(){
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[0],
+    );
+  }
+}
+

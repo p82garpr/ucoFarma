899c667bf36a98729ba7281c284ef3f0906b3406
fjavifp4 <fjavifp4@gmail.com>
2024-11-27 13:15:44 +0100

    owned medicines

--- frontend/uco_farma/lib/src/domain/services/chat_service.dart
+++ frontend/uco_farma/lib/src/domain/services/chat_service.dart
@@ -1,11 +1,35 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/medicine_model.dart';
 
 class ChatService {
-  final String _baseUrl = 'http://10.0.2.2:1234/v1/chat/completions'; // URL de LMStudio
+  final String _baseUrl = 'http://10.0.2.2:1234/v1/chat/completions';
 
-  Future<String> sendMessage(String message) async {
+  Future<String> sendMessage(String message, List<Medicine>? medicines) async {
     try {
+      String medicinesContext = '';
+      if (medicines != null && medicines.isNotEmpty) {
+        medicinesContext = 'Los medicamentos actuales del usuario son:\n';
+        for (var medicine in medicines) {
+          String doseInfo = '';
+          if (medicine.doses?.isNotEmpty ?? false) {
+            final dose = medicine.doses!.first;
+            if (dose != null) {
+              doseInfo = '''
+             Frecuencia: ${dose.frequency} veces al día
+             Cantidad por dosis: ${dose.quantity}''';
+            }
+          }
+
+          medicinesContext += '''- ${medicine.name} (CN: ${medicine.cn})
+             Cantidad: ${medicine.quantity} dosis
+             Tipo: ${medicine.type}
+             $doseInfo\n''';
+        }
+      }
+
+      print('Contexto de medicamentos: $medicinesContext');
+
       final response = await http.post(
         Uri.parse(_baseUrl),
         headers: {
@@ -15,7 +39,14 @@ class ChatService {
           "messages": [
             {
               "role": "system",
-              "content": "Eres un asistente médico experto en medicamentos. Proporciona información precisa y útil sobre medicamentos, sus usos, efectos secundarios y precauciones. No des consejos médicos directos, solo información general."
+              "content": """Eres un asistente médico experto en medicamentos. 
+              Tu función es ayudar con información sobre los medicamentos del usuario.
+              
+              $medicinesContext
+              
+              Proporciona información precisa y útil sobre los medicamentos listados,
+              sus usos, efectos secundarios y precauciones. No des consejos médicos directos,
+              solo información general y recordatorios sobre las dosis registradas."""
             },
             {
               "role": "user",
